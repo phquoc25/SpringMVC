53ed225c729fd4ef165f3c477ddabc284339580e
Ho Quoc PHAN <phquoc25@gmail.com>
2017-01-17 05:34:34 +0100

    Git auth test

--- src/main/java/com/qph/common/controller/MovieController.java
+++ src/main/java/com/qph/common/controller/MovieController.java
@@ -11,7 +11,6 @@ public class MovieController {
 	
 	@RequestMapping(value="/{name}", method=RequestMethod.GET)
 	public String getMovie(String name, ModelMap model) {
-		System.out.print("Git auth test");
 		return "list";
 	}
 }
