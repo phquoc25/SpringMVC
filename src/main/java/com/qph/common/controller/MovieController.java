1db1047b1618e6d293c9b68d87e939d1574e850a
Quoc PHAN <phquoc25@gmail.com>
2018-05-06 15:04:45 +0700

    Using spring boot

--- src/main/java/com/qph/common/controller/MovieController.java
+++ src/main/java/com/qph/common/controller/MovieController.java
@@ -1,16 +1,15 @@
 package com.qph.common.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/movie")
+@RestController("/movie")
 public class MovieController {
 	
 	@RequestMapping(value="/{name}", method=RequestMethod.GET)
 	public String getMovie(String name, ModelMap model) {
-		return "list";
+		return name;
 	}
 }
